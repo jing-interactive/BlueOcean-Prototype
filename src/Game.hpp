80113373915a3e460c17e3487efa224bb7e91ab3
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2016-08-06 01:17:38 +0900

    Gameから関数を追い出した

--- src/Game.hpp
+++ src/Game.hpp
@@ -294,50 +294,6 @@ class Game {
   }
 #endif
 
-
-  std::pair<bool, float> intersect(const ci::Ray& ray, const ci::TriMesh& mesh) {
-    const auto& vertex = mesh.getPositions<3>();
-    const auto& indicies = mesh.getIndices();
-
-    bool  cross       = false;
-    float cross_min_z = std::numeric_limits<float>::max();
-    
-    for (u_int i = 0; i < indicies.size(); i += 3) {
-      float cross_z;
-      if (ray.calcTriangleIntersection(vertex[indicies[i]], vertex[indicies[i + 1]], vertex[indicies[i + 2]], &cross_z)) {
-        cross = true;
-        cross_min_z = std::min(cross_z, cross_min_z);
-      }
-    }
-
-    return std::make_pair(cross, cross_min_z);
-  }
-
-  std::tuple<bool, float, ci::vec3> intersect(const ci::Ray& ray, const std::vector<Relic>& relics) {
-    bool  cross       = false;
-    float cross_min_z = std::numeric_limits<float>::max();
-    ci::vec3 cross_pos;
-
-    for (const auto& relic : relics) {
-      ci::vec3 p(relic.position);
-      // 遺物のマーカーは海上に浮いている
-      p.y = std::max(p.y, sea_level_);
-      
-      ci::AxisAlignedBox aabb(p, p + ci::vec3(1, 1, 1));
-
-      float min_z, max_z;
-      if (aabb.intersect(ray, &min_z, &max_z) > 0) {
-        cross = true;
-        if (min_z < cross_min_z) {
-          cross_min_z = min_z;
-          cross_pos   = p;
-        }
-      }
-    }
-    
-    return std::make_tuple(cross, cross_min_z, cross_pos);
-  }
-
   
   void pickStage(const ci::vec2& pos) {
     // スクリーン座標→正規化座標
@@ -391,7 +347,7 @@ class Game {
         }
 
         // 遺物を直接クリックしてるか調べる
-        auto relic_cross = intersect(t_ray, stage.getRelics(ci::ivec2(x, z)));
+        auto relic_cross = intersect(t_ray, stage.getRelics(ci::ivec2(x, z)), sea_level_);
         if (std::get<0>(relic_cross) && (std::get<1>(relic_cross) < cross_min_z)) {
           picked_ = true;
           cross_min_z = std::get<1>(relic_cross);
--- src/Misc.hpp
+++ src/Misc.hpp
@@ -20,4 +20,48 @@ struct LessVec {
   }
 };
 
+
+std::pair<bool, float> intersect(const ci::Ray& ray, const ci::TriMesh& mesh) {
+  const auto& vertex = mesh.getPositions<3>();
+  const auto& indicies = mesh.getIndices();
+
+  bool  cross       = false;
+  float cross_min_z = std::numeric_limits<float>::max();
+    
+  for (u_int i = 0; i < indicies.size(); i += 3) {
+    float cross_z;
+    if (ray.calcTriangleIntersection(vertex[indicies[i]], vertex[indicies[i + 1]], vertex[indicies[i + 2]], &cross_z)) {
+      cross = true;
+      cross_min_z = std::min(cross_z, cross_min_z);
+    }
+  }
+
+  return std::make_pair(cross, cross_min_z);
+}
+
+std::tuple<bool, float, ci::vec3> intersect(const ci::Ray& ray, const std::vector<Relic>& relics, const float sea_level) {
+  bool  cross       = false;
+  float cross_min_z = std::numeric_limits<float>::max();
+  ci::vec3 cross_pos;
+
+  for (const auto& relic : relics) {
+    ci::vec3 p(relic.position);
+    // 遺物のマーカーは海上に浮いている
+    p.y = std::max(p.y, sea_level);
+      
+    ci::AxisAlignedBox aabb(p, p + ci::vec3(1, 1, 1));
+
+    float min_z, max_z;
+    if (aabb.intersect(ray, &min_z, &max_z) > 0) {
+      cross = true;
+      if (min_z < cross_min_z) {
+        cross_min_z = min_z;
+        cross_pos   = p;
+      }
+    }
+  }
+    
+  return std::make_tuple(cross, cross_min_z, cross_pos);
+}
+
 }
